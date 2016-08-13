376bc63d2cb39f3ac122533dbd34ed2688eae6fb
Miguel Torres <prototypethis@Miguels-MacBook-Pro.local>
2016-08-13 13:00:56 -0700

    Player capsule added
    player capsule, terrain, and player script added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,22 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	#region playerVariables
+	public int maxHunger;
+	public int hunger;
+	public int hungerIncreaseRate;
+
+	public int health;
+	public int maxHealth;
+
+	public int fuel;
+	public int maxFuel;
+
+	public int energy;
+	public int maxEnergy;
+
+	#endregion
+
 	// Use this for initialization
 	void Start () {
 	
