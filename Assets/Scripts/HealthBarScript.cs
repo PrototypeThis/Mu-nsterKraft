cc75aeee873c2b4cfcf0fac6654f78f06a59411c
Miguel Torres <prototypethis@Miguels-MacBook-Pro.local>
2016-08-18 19:35:00 -0700

    MünsterKraft
    Added player Input, UI, rock and wood models.

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarScript : MonoBehaviour {
+
+    public float inset = 40.0f;
+    public float size = 138.0f;
+
+    RectTransform thisRectTransform;
+	void Start ()
+    {
+        thisRectTransform = gameObject.GetComponent<RectTransform>();
+	}
+	
+	void Update ()
+    {
+        //gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+        thisRectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, inset, size);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour 
+{
+	public enum PlayerState
+	{
+		Attack,
+		Eat,
+		Sleep,
+		Spell,
+		SuckBlood,
 
-	#region playerVariables
-	public int maxHunger;
-	public int hunger;
-	public int hungerIncreaseRate;
 
-	public int health;
-	public int maxHealth;
-
-	public int fuel;
-	public int maxFuel;
-
-	public int energy;
-	public int maxEnergy;
-
-	#endregion
-
-	// Use this for initialization
-	void Start () {
-	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public float MaxHP;
+	public float HP;
+	public float MaxMana;		//Mana, magic, etc.
+	public float Mana;
+
+	public float MaxHunger;		
+	public float Hunger;
+	public float HungeringRate;
+
+	public float MaxStamina;
+	public float Stamina;
+
+	public float speed = 20.0F;
+	public float jumpSpeed = 20.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+
+
+	void Update() 
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) 
+		{
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
 	}
+
 }
+
